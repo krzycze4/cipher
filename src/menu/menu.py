e17db51af585b7f05f2629070a371551eb4102c2
Krzychu <czernicki.krz@gmail.com>
2023-08-10 12:03:02 +0200

    feat(menu): add new methods - display_welcome, display_main_menu

---
+++ src/menu/menu.py
@@ -0,0 +1,14 @@
+class Menu:
+
+    @staticmethod
+    def display_welcome() -> None:
+        print("\nWelcome to Cipher")
+
+    @staticmethod
+    def display_main_menu() -> None:
+        print("\nMenu:")
+        print("1. Encrypt text")
+        print("2. Decrypt text")
+        print("3. Save buffer")
+        print("4. Load buffer")
+        print("5. Exit")
