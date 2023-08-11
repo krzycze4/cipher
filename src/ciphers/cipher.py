1d6bb0fe4d899bb36346a02425a9c898f1e16f29
Krzychu <czernicki.krz@gmail.com>
2023-08-11 12:39:37 +0200

    feat(cipher): create __str__ to take name of using cipher

--- src/ciphers/cipher.py
+++ src/ciphers/cipher.py
@@ -2,6 +2,9 @@ from abc import ABC, abstractmethod
 
 
 class Cipher(ABC):
+    def __str__(self):
+        return f"{self.__class__.__name__}"
+
     @abstractmethod
     def encrypt(self, text_content: str) -> str:
         raise NotImplementedError
