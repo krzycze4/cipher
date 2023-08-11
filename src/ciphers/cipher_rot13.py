1b21afa653ac4f51709a6171c9bddb52eb2beeea
Krzychu <czernicki.krz@gmail.com>
2023-08-11 12:36:54 +0200

    fix(cipher_rot13): remove bug in methods encrypt and decrypt - connected with setting encrypted and decrypted letter

--- src/ciphers/cipher_rot13.py
+++ src/ciphers/cipher_rot13.py
@@ -9,11 +9,11 @@ class CipherROT13(Cipher):
             if char_decimal in range(ord("A"), ord("Z") + 1):
                 char_decimal = char_decimal + 13
                 if char_decimal > ord("Z"):
-                    char_decimal = ord("A") + char_decimal - ord("Z")
+                    char_decimal = ord("A") + char_decimal - ord("Z") - 1
             elif char_decimal in range(ord("a"), ord("z") + 1):
                 char_decimal = char_decimal + 13
                 if char_decimal > ord("z"):
-                    char_decimal = ord("a") + char_decimal - ord("a")
+                    char_decimal = ord("a") + char_decimal - ord("z") - 1
             encrypted_chars.append(chr(char_decimal))
         output = "".join(encrypted_chars)
         return output
@@ -25,11 +25,11 @@ class CipherROT13(Cipher):
             if char_decimal in range(ord("A"), ord("Z") + 1):
                 char_decimal = char_decimal - 13
                 if char_decimal < ord("A"):
-                    char_decimal = ord("Z") - char_decimal + ord("A")
+                    char_decimal = ord("Z") - char_decimal + ord("A") + 1
             elif char_decimal in range(ord("a"), ord("z") + 1):
                 char_decimal = char_decimal - 13
                 if char_decimal < ord("a"):
-                    char_decimal = ord("z") - char_decimal + ord("a")
+                    char_decimal = ord("z") - char_decimal + ord("a") + 1
             encrypted_chars.append(chr(char_decimal))
         output = "".join(encrypted_chars)
         return output
