4e23e497287b0c2e6e68c3b8821642304725999b
Krzychu <czernicki.krz@gmail.com>
2023-08-11 13:16:57 +0200

    feat(cipher_manager): create 'encrypt_text' method, change name of the field self.menu_options, add self.cipher_options dict and add self.buffer list

--- src/manager/cipher_manager.py
+++ src/manager/cipher_manager.py
@@ -1,3 +1,6 @@
+from src.ciphers.cipher_rot13 import CipherROT13
+from src.ciphers.cipher_rot47 import CipherROT47
+from src.factories.text_factory import TextFactory
 from src.manager.manager import Manager
 from src.menu.menu import Menu
 
@@ -5,36 +8,61 @@ from src.menu.menu import Menu
 class CipherManager(Manager):
     def __init__(self):
         self.menu = Menu()
-        self.options = {
+        self.cipher_rot13 = CipherROT13()
+        self.cipher_rot47 = CipherROT47()
+        self.menu_options = {
             1: self.encrypt_text,
             2: self.decrypt_text,
             3: self.save_buffer,
             4: self.load_to_buffer,
             5: self.exit
         }
+        self.cipher_options = {
+            1: self.cipher_rot13,
+            2: self.cipher_rot47
+        }
+        self.buffer = []
 
     def run(self):
         self.menu.display_welcome()
         while True:
             self.menu.display_main_menu()
-            self.menu.take_choice()
+            self.menu.take_choice(limit=5) #TODO - zmień na len(dict)
             self.execute()
 
     def execute(self):
-        self.options.get(self.menu.choice)()
+        self.menu_options.get(self.menu.choice)()
 
     def encrypt_text(self):
-        pass
+        user_input = input("\nText content: ")
+        self.menu.display_cipher_menu()
+        self.menu.take_choice(limit=2) #TODO - zmień na len(dict)
+        content = self.cipher_options.get(self.menu.choice).encrypt(user_input)
+        status = "encrypted"
+        rot_type = str(self.cipher_options.get(self.menu.choice))
+        text = TextFactory().create_object(content, rot_type, status)
+        self.buffer.append(text)
 
     def decrypt_text(self):
+        # tutaj będziesz pobierał content
+        # pobierał rot
+        # użyj szyfru - TODO DECRYPTION
+        # określał status
+        # użyjesz tutaj factory
+        # dodasz do buffer
         pass
 
     def save_buffer(self):
+        # zapytaj o nazwę pliku do zapisu
+        # context manager z jsonem, flaga "a"
+        # zapis
         pass
 
     def load_to_buffer(self):
+        # zapytaj o nazwę pliku do zapisu
+        # context manager z jsonem, flaga "a"
+        # odczyt do buffera
         pass
 
     def exit(self):
         exit()
-
