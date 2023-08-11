793a1a59219154086f7ee33b52ab5c0c5860939e
Krzychu <czernicki.krz@gmail.com>
2023-08-11 11:25:02 +0200

    feat(cipher_rot13): add new methods - encrypt, decrypt

---
+++ src/ciphers/cipher_rot13.py
@@ -0,0 +1,35 @@
+from src.ciphers.cipher import Cipher
+
+
+class CipherROT13(Cipher):
+    def encrypt(self, text_content: str) -> str:
+        encrypted_chars = []
+        for char in text_content:
+            char_decimal = ord(char)
+            if char_decimal in range(ord("A"), ord("Z") + 1):
+                char_decimal = char_decimal + 13
+                if char_decimal > ord("Z"):
+                    char_decimal = ord("A") + char_decimal - ord("Z")
+            elif char_decimal in range(ord("a"), ord("z") + 1):
+                char_decimal = char_decimal + 13
+                if char_decimal > ord("z"):
+                    char_decimal = ord("a") + char_decimal - ord("a")
+            encrypted_chars.append(chr(char_decimal))
+        output = "".join(encrypted_chars)
+        return output
+
+    def decrypt(self, text_content: str) -> str:
+        encrypted_chars = []
+        for char in text_content:
+            char_decimal = ord(char)
+            if char_decimal in range(ord("A"), ord("Z") + 1):
+                char_decimal = char_decimal - 13
+                if char_decimal < ord("A"):
+                    char_decimal = ord("Z") - char_decimal + ord("A")
+            elif char_decimal in range(ord("a"), ord("z") + 1):
+                char_decimal = char_decimal - 13
+                if char_decimal < ord("a"):
+                    char_decimal = ord("z") - char_decimal + ord("a")
+            encrypted_chars.append(chr(char_decimal))
+        output = "".join(encrypted_chars)
+        return output
