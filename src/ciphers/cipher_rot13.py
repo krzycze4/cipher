74fc38ff1ccf8917ecfeac5ffe7d08376e8f27ac
Krzychu <czernicki.krz@gmail.com>
2023-08-12 13:49:04 +0200

    fix(cipher_rot13): fix bug with method decrypt

--- src/ciphers/cipher_rot13.py
+++ src/ciphers/cipher_rot13.py
@@ -1,6 +1,7 @@
 from src.ciphers.cipher import Cipher
 
 
+# TODO wydzielić wspólną logikę
 class CipherROT13(Cipher):
     def encrypt(self, text_content: str) -> str:
         encrypted_chars = []
@@ -24,12 +25,12 @@ class CipherROT13(Cipher):
             char_decimal = ord(char)
             if char_decimal in range(ord("A"), ord("Z") + 1):
                 char_decimal = char_decimal - 13
-                if char_decimal > ord("Z"):
+                if char_decimal < ord("A"):
                     char_decimal = ord("Z") - char_decimal + ord("A") + 1
             elif char_decimal in range(ord("a"), ord("z") + 1):
                 char_decimal = char_decimal - 13
                 if char_decimal < ord("a"):
-                    char_decimal = ord("z") - char_decimal + ord("a") + 1
+                    char_decimal = ord("z") + char_decimal - ord("a") + 1
             encrypted_chars.append(chr(char_decimal))
         output = "".join(encrypted_chars)
         return output
