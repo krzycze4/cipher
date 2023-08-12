b28570db41bc2f07e703d0e32de6b14837d96494
Krzychu <czernicki.krz@gmail.com>
2023-08-12 16:34:33 +0200

    refactor(cipher_ro13): refactor class

--- src/ciphers/cipher_rot13.py
+++ src/ciphers/cipher_rot13.py
@@ -1,36 +1,43 @@
 from src.ciphers.cipher import Cipher
 
 
-# TODO wydzielić wspólną logikę
-class CipherROT13(Cipher):
-    def encrypt(self, text_content: str) -> str:
-        encrypted_chars = []
-        for char in text_content:
-            char_decimal = ord(char)
-            if char_decimal in range(ord("A"), ord("Z") + 1):
-                char_decimal = char_decimal + 13
-                if char_decimal > ord("Z"):
-                    char_decimal = ord("A") + char_decimal - ord("Z") - 1
-            elif char_decimal in range(ord("a"), ord("z") + 1):
-                char_decimal = char_decimal + 13
-                if char_decimal > ord("z"):
-                    char_decimal = ord("a") + char_decimal - ord("z") - 1
-            encrypted_chars.append(chr(char_decimal))
-        output = "".join(encrypted_chars)
-        return output
+UPPER_RANGE = range(ord("A"), ord("Z") + 1)
+LOWER_RANGE = range(ord("a"), ord("z") + 1)
 
-    def decrypt(self, text_content: str) -> str:
+
+class CipherROT13(Cipher):
+
+    @classmethod
+    def encrypt(cls, text_content: str) -> str:
+        return cls.create_new_string(text_content=text_content, char_shift=13)
+
+    @classmethod
+    def decrypt(cls, text_content: str) -> str:
+        return cls.create_new_string(text_content=text_content, char_shift=-13)
+
+    @classmethod
+    def create_new_string(cls, text_content: str, char_shift: int) -> str:
         encrypted_chars = []
         for char in text_content:
-            char_decimal = ord(char)
-            if char_decimal in range(ord("A"), ord("Z") + 1):
-                char_decimal = char_decimal - 13
-                if char_decimal < ord("A"):
-                    char_decimal = ord("Z") - char_decimal + ord("A") + 1
-            elif char_decimal in range(ord("a"), ord("z") + 1):
-                char_decimal = char_decimal - 13
-                if char_decimal < ord("a"):
-                    char_decimal = ord("z") + char_decimal - ord("a") + 1
-            encrypted_chars.append(chr(char_decimal))
-        output = "".join(encrypted_chars)
-        return output
+            if ord(char) in UPPER_RANGE:
+                encrypted_chars.append(cls.shift_char(char=char,
+                                                      char_shift=char_shift,
+                                                      letter_a="A",
+                                                      letter_z="Z"))
+            elif ord(char) in LOWER_RANGE:
+                encrypted_chars.append(cls.shift_char(char=char,
+                                                      char_shift=char_shift,
+                                                      letter_a="a",
+                                                      letter_z="z"))
+        return "".join(encrypted_chars)
+
+    @staticmethod
+    def shift_char(char: str, char_shift: int, letter_a: str, letter_z: str) -> str:
+        char_decimal = ord(char)
+        if char_decimal in range(ord(letter_a), ord(letter_z) + 1):
+            char_decimal = char_decimal + char_shift
+            if char_decimal > ord(letter_z):
+                char_decimal = ord(letter_a) + char_decimal - ord(letter_z) - 1
+            elif char_decimal < ord(letter_a):
+                char_decimal = ord(letter_z) - (ord(letter_a) - char_decimal) + 1
+        return chr(char_decimal)
