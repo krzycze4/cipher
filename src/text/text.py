c0beefffc9e888f7f8e1d064d0930bc52e34d851
Krzychu <czernicki.krz@gmail.com>
2023-08-12 11:34:54 +0200

    feat(text): add methods(content, rot_type, status) with decorator proprety

--- src/text/text.py
+++ src/text/text.py
@@ -7,3 +7,15 @@ class Text:
     _content: Union[None, str] = None
     _rot_type: Union[None, str] = None
     _status: Union[None, str] = None
+
+    @property
+    def content(self):
+        return self._content
+
+    @property
+    def rot_type(self):
+        return self._rot_type
+
+    @property
+    def status(self):
+        return self._status
