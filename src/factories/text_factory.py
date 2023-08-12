edc5e137375964fb2a12beaeb54167d5342d360d
Krzychu <czernicki.krz@gmail.com>
2023-08-12 15:36:52 +0200

    refactor(text_factory): set create_object method as classmethod

--- src/factories/text_factory.py
+++ src/factories/text_factory.py
@@ -3,5 +3,6 @@ from src.text.text import Text
 
 
 class TextFactory(AbstractFactory):
-    def create_object(self, content: str, rot_type: str, status: str) -> Text:
+    @classmethod
+    def create_object(cls, content: str, rot_type: str, status: str) -> Text:
         return Text(_content=content, _rot_type=rot_type, _status=status)
