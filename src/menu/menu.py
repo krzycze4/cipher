4f038c44fa7b848e1a7ec441bda0cbb73d93aeb0
Krzychu <czernicki.krz@gmail.com>
2023-08-12 12:09:42 +0200

    feat(menu): delete method take_choice

--- src/menu/menu.py
+++ src/menu/menu.py
@@ -1,24 +1,4 @@
-from typing import Union
-
-from src.exceptions.exceptions import OutOfRangeError
-
-
 class Menu:
-    def __init__(self) -> None:
-        self.choice: Union[None, int] = None
-
-    def take_choice(self, limit: int) -> None:
-        while True:
-            try:
-                choice = int(input("\nChoice: "))
-                if choice not in range(1, limit + 1):
-                    raise OutOfRangeError
-            except (ValueError, OutOfRangeError):
-                print(f"Only numbers from 1 to {limit}")
-                self.choice = None
-            else:
-                self.choice = choice
-                break
 
     @staticmethod
     def display_welcome() -> None:
