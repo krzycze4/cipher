cd64ba8d183c5692224e38d259095c0c62c4ed03
Krzychu <czernicki.krz@gmail.com>
2023-08-12 17:01:20 +0200

    feat(file_handler): add *args to methods - save_to_file and load_from_file

--- src/file_handlers/file_handler.py
+++ src/file_handlers/file_handler.py
@@ -3,9 +3,9 @@ from abc import ABC, abstractmethod
 
 class FileHandler(ABC):
     @abstractmethod
-    def save_to_file(self):
+    def save_to_file(self, *args):
         raise NotImplementedError
 
     @abstractmethod
-    def load_from_file(self):
+    def load_from_file(self, *args):
         raise NotImplementedError
