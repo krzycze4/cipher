b2c8642a87be6d4d1dd73e65b986c324ee65e793
Krzychu <czernicki.krz@gmail.com>
2023-08-12 15:25:37 +0200

    refactor(cipher_rot47): extract common logic from methods encrypt and decrypt to method shift_char

--- src/ciphers/cipher_rot47.py
+++ src/ciphers/cipher_rot47.py
@@ -5,23 +5,23 @@ class CipherROT47(Cipher):
     def encrypt(self, text_content: str) -> str:
         encrypted_chars = []
         for char in text_content:
-            char_decimal = ord(char)
-            if char_decimal in range(ord("!"), ord("~") + 1):
-                char_decimal = char_decimal + 47
-                if char_decimal > ord("~"):
-                    char_decimal = ord("!") + char_decimal - ord("~") - 1
-            encrypted_chars.append(chr(char_decimal))
-        output = "".join(encrypted_chars)
-        return output
+            encrypted_chars.append(self.shift_char(char=char, char_shift=47))
+        return "".join(encrypted_chars)
 
     def decrypt(self, text_content: str) -> str:
         encrypted_chars = []
         for char in text_content:
-            char_decimal = ord(char)
-            if char_decimal in range(ord("!"), ord("~") + 1):
-                char_decimal = char_decimal - 47
-                if char_decimal < ord("!"):
-                    char_decimal = ord("~") - ord("!") + char_decimal + 1
-            encrypted_chars.append(chr(char_decimal))
+            encrypted_chars.append(self.shift_char(char=char, char_shift=-47))
         output = "".join(encrypted_chars)
         return output
+
+    @staticmethod
+    def shift_char(char: str, char_shift: int) -> str:
+        char_decimal = ord(char)
+        if char_decimal in range(ord("!"), ord("~") + 1):
+            char_decimal = char_decimal + char_shift
+            if char_decimal > ord("~"):
+                char_decimal = ord("!") + char_decimal - ord("~") - 1
+            elif char_decimal < ord("!"):
+                char_decimal = ord("~") - (ord("!") - char_decimal) + 1
+        return chr(char_decimal)
