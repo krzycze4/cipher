f1948973d5af475d45d2f7e102e8994e5ccf59d6
Krzychu <czernicki.krz@gmail.com>
2023-08-12 14:30:44 +0200

    refactor(cipher_manager): extract common logic methods decrypt_text and encrypt_text to method crypt_text and create method take_input_content to validate input

--- src/manager/cipher_manager.py
+++ src/manager/cipher_manager.py
@@ -2,7 +2,7 @@ from typing import Union
 
 from src.ciphers.cipher_rot13 import CipherROT13
 from src.ciphers.cipher_rot47 import CipherROT47
-from src.exceptions.exceptions import OutOfRangeError
+from src.exceptions.exceptions import OutOfRangeError, EmptyInputError
 from src.factories.text_factory import TextFactory
 from src.manager.manager import Manager
 from src.menu.menu import Menu
@@ -11,12 +11,16 @@ from src.menu.menu import Menu
 class CipherManager(Manager):
     def __init__(self):
         self.menu = Menu()
+
         self.choice: Union[None, int] = None
+        self.content_input: Union[None, str] = None
+
         self.cipher_rot13 = CipherROT13()
         self.cipher_rot47 = CipherROT47()
+        self.buffer = []
         self.menu_options = {
-            1: self.encrypt_text,
-            2: self.decrypt_text,
+            1: self.crypt_text,
+            2: self.crypt_text,
             3: self.save_buffer,
             4: self.load_to_buffer,
             5: self.exit
@@ -25,7 +29,14 @@ class CipherManager(Manager):
             1: self.cipher_rot13,
             2: self.cipher_rot47
         }
-        self.buffer = []
+        self.status = {
+            1: "encrypted",
+            2: "decrypted"
+        }
+        self.crypt_options = {
+            1: self.encrypt_text,
+            2: self.decrypt_text
+        }
 
     def run(self):
         self.menu.display_welcome()
@@ -50,27 +61,35 @@ class CipherManager(Manager):
     def execute(self):
         self.menu_options.get(self.choice)()
 
-    def encrypt_text(self):
-        user_input = input("\nText content: ")
+    def crypt_text(self) -> None:
+        self.take_input_content()
         self.menu.display_cipher_menu()
         self.take_choice(limit=len(self.cipher_options))
-        content = self.cipher_options.get(self.choice).encrypt(text_content=user_input)
-        status = "encrypted"
+
         rot_type = str(self.cipher_options.get(self.choice))
+        status = self.status.get(self.choice)
+        content = self.crypt_options.get(self.choice)()
         text = TextFactory().create_object(content=content, rot_type=rot_type, status=status)
+
         print(text.content)
         self.buffer.append(text)
 
-    def decrypt_text(self):
-        user_input = input("\nText content: ")
-        self.menu.display_cipher_menu()
-        self.take_choice(limit=len(self.cipher_options))
-        content = self.cipher_options.get(self.choice).decrypt(text_content=user_input)
-        status = "decrypted"
-        rot_type = str(self.cipher_options.get(self.choice))
-        text = TextFactory().create_object(content=content, rot_type=rot_type, status=status)
-        print(text.content)
-        self.buffer.append(text)
+    def take_input_content(self):
+        while True:
+            try:
+                user_input = input("\nText content: ")
+                if len(user_input.strip()) == 0:
+                    raise EmptyInputError
+            except EmptyInputError:
+                print("Empty input")
+            else:
+                self.content_input = user_input
+
+    def encrypt_text(self) -> str:
+        return self.cipher_options.get(self.choice).encrypt(text_content=self.content_input)
+
+    def decrypt_text(self) -> str:
+        return self.cipher_options.get(self.choice).decrypt(text_content=self.content_input)
 
     def save_buffer(self):
         # zapytaj o nazwę pliku do zapisu
