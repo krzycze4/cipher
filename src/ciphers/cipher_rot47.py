3cd3ebe207720e63966281acdc4daa0c8252ab0e
Krzychu <czernicki.krz@gmail.com>
2023-08-12 16:35:41 +0200

    refactor(cipher_rot47): refactor class

--- src/ciphers/cipher_rot47.py
+++ src/ciphers/cipher_rot47.py
@@ -1,26 +1,29 @@
 from src.ciphers.cipher import Cipher
 
 
+CIPHER_RANGE = range(ord("!"), ord("~") + 1)
+
+
 class CipherROT47(Cipher):
     @classmethod
     def encrypt(cls, text_content: str) -> str:
-        encrypted_chars = []
-        for char in text_content:
-            encrypted_chars.append(cls.shift_char(char=char, char_shift=47))
-        return "".join(encrypted_chars)
+        return cls.create_new_string(text_content=text_content, char_shift=47)
 
     @classmethod
     def decrypt(cls, text_content: str) -> str:
+        return cls.create_new_string(text_content=text_content, char_shift=-47)
+
+    @classmethod
+    def create_new_string(cls, text_content: str, char_shift: int) -> str:
         encrypted_chars = []
         for char in text_content:
-            encrypted_chars.append(cls.shift_char(char=char, char_shift=-47))
-        output = "".join(encrypted_chars)
-        return output
+            encrypted_chars.append(cls.shift_char(char=char, char_shift=char_shift))
+        return "".join(encrypted_chars)
 
     @staticmethod
     def shift_char(char: str, char_shift: int) -> str:
         char_decimal = ord(char)
-        if char_decimal in range(ord("!"), ord("~") + 1):
+        if char_decimal in CIPHER_RANGE:
             char_decimal = char_decimal + char_shift
             if char_decimal > ord("~"):
                 char_decimal = ord("!") + char_decimal - ord("~") - 1
