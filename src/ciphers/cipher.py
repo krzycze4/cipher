e148687f3a8e14d32cba679d694e757d4766a863
Krzychu <czernicki.krz@gmail.com>
2023-08-13 15:00:59 +0200

    docs(cipher): create docstrings

--- src/ciphers/cipher.py
+++ src/ciphers/cipher.py
@@ -2,13 +2,26 @@ from abc import ABC, abstractmethod
 
 
 class Cipher(ABC):
+    """
+    This is Abstract Class Cipher
+    """
     def __str__(self):
         return f"{self.__class__.__name__}"
 
     @abstractmethod
     def encrypt(self, text_content: str) -> str:
+        """
+        This is abstract method. Encrypt parameter text_content.
+        :param text_content: str
+        :raise: NotImplementedError
+        """
         raise NotImplementedError
 
     @abstractmethod
     def decrypt(self, text_content: str) -> str:
+        """
+        This is abstract method. Decrypt parameter text_content.
+        :param text_content: str
+        :return: NotImplementedError
+        """
         raise NotImplementedError
