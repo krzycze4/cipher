cd577eb8d67a919d5f69bea215a03e2117b4ce0f
Krzychu <czernicki.krz@gmail.com>
2023-08-14 11:29:30 +0200

    docs(text_factory): add docstrings

--- src/factories/text_factory.py
+++ src/factories/text_factory.py
@@ -3,6 +3,18 @@ from src.texts.text import Text
 
 
 class TextFactory(AbstractFactory):
+    """
+    Class is Abstract Factory.
+    Inheritance from AbstractFactory class.
+    """
     @classmethod
     def create_object(cls, content: str, rot_type: str, status: str) -> Text:
+        """
+        It's class method.
+        Create and return Text object.
+        :param content: str
+        :param rot_type: str
+        :param status: str
+        :return: Text
+        """
         return Text(_content=content, _rot_type=rot_type, _status=status)
