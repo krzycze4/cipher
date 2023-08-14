7dd19cd538f8c81a2204cb14ee9d643f9a78e239
Krzychu <czernicki.krz@gmail.com>
2023-08-14 11:20:40 +0200

    docs(abstract_factory): create docstrings

--- src/factories/abstract_factory.py
+++ src/factories/abstract_factory.py
@@ -2,6 +2,9 @@ from abc import ABC, abstractmethod
 
 
 class AbstractFactory(ABC):
+    """
+    Abstract Factory Class
+    """
     @abstractmethod
     def create_object(self, *args):
         raise NotImplementedError
