1c16ee6bd29f0485d30e5e659691f9f138fc0f5a
Krzychu <czernicki.krz@gmail.com>
2023-08-14 11:18:56 +0200

    docs(exceptions): create docstrings

--- src/exceptions/exceptions.py
+++ src/exceptions/exceptions.py
@@ -1,6 +1,12 @@
 class OutOfRangeError(Exception):
+    """
+    Exception used when user choice is out of range.
+    """
     ...
 
 
 class EmptyInputError(Exception):
+    """
+    Exception used when user input is empty.
+    """
     ...
