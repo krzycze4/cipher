bfcab257cb4942bcafa106cd560f4b02ab6a8608
Krzychu <czernicki.krz@gmail.com>
2023-08-15 13:21:16 +0200

    docs(manager): create docstrings

--- src/managers/manager.py
+++ src/managers/manager.py
@@ -1,11 +1,41 @@
+"""
+A module to represent an abstract class Manager.
+
+Classes:
+    Manager(ABC)
+"""
 from abc import ABC, abstractmethod
 
 
 class Manager(ABC):
+    """
+    An abstract class to represent a manager.
+
+    Methods
+    _______
+    run()
+        raises NotImplementedError
+    exit()
+        raises NotImplementedError
+    """
     @abstractmethod
     def run(self):
+        """
+        An abstract method raises NotImplementedError
+
+        Raises
+        ______
+        NotImplementedError
+        """
         raise NotImplementedError
 
     @abstractmethod
     def exit(self):
+        """
+        An abstract method raises NotImplementedError
+
+        Raises
+        ______
+        NotImplementedError
+        """
         raise NotImplementedError
