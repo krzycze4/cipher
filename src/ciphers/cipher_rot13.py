d0d3aa7689fb3dd0c0a149c3a7460b3b34cf06d8
Krzychu <czernicki.krz@gmail.com>
2023-08-15 11:06:55 +0200

    docs(cipher_rot13): create correct docstrings

--- src/ciphers/cipher_rot13.py
+++ src/ciphers/cipher_rot13.py
@@ -1,16 +1,32 @@
+"""
+A module to represent a class of cipher ROT13.
+"Shift" means shift in ASCII encoding system.
+
+Classes:
+    CipherROT13(Cipher)
+"""
 from src.ciphers.cipher import Cipher
 
 
 class CipherROT13(Cipher):
     """
-    This is class CipherROT13.
-    Inheritance from abstract class Cipher.
-    Rotate characters in word by 13
-    in forward(encrypt) or backward(decrypt).
-    Example: "A" -> "N",
-             "a" -> "n",
-             "N -> "A",
-             "n" -> "a"
+    A class to represent a cipher ROT13.
+
+    Variables
+    _______________
+    upper_range
+    lower_range
+
+    Methods
+    _______
+    encrypt(text_content: str) -> str
+        a class method returns encrypted text
+    decrypt(text_content: str) -> str
+        a class method returns decrypted text
+    create_new_string(text_content: str, char_shift: int) -> str
+        a class method returns crypted or decrypted text
+    shift_char(char: str, char_shift: int, letter_a: str, letter_z: str) -> str
+        a static method returns shifted character
     """
     upper_range = range(ord("A"), ord("Z") + 1)
     lower_range = range(ord("a"), ord("z") + 1)
@@ -18,60 +34,89 @@ class CipherROT13(Cipher):
     @classmethod
     def encrypt(cls, text_content: str) -> str:
         """
-        This is class method.
-        Encrypt parameter text_content and return encrypted string.
-        :param text_content: str.
-        :return: str
+        A class method returns encrypted text.
+
+        Parameters
+        __________
+        text_content: str
+            text to encrypt
+
+        Returns
+        _______
+        str
+            encrypted text
         """
         return cls.create_new_string(text_content=text_content, char_shift=13)
 
     @classmethod
     def decrypt(cls, text_content: str) -> str:
         """
-        This is class method.
-        Decrypt parameter text_content and return decrypted string.
-        :param text_content: str.
-        :return: str
+        A class method returns decrypted text.
+
+        Parameters
+        __________
+        text_content: str
+            text to decrypt
+
+        Returns
+        _______
+        str
+            decrypted text
         """
         return cls.create_new_string(text_content=text_content, char_shift=-13)
 
     @classmethod
     def create_new_string(cls, text_content: str, char_shift: int) -> str:
         """
-        This is class method.
-        Method takes parameter text_content and shift particular letter in
-        text_content by char_shift. Then create and return new string.
-        :param text_content: str
-        :param char_shift: int
-        :return: str
+        A class method returns crypted or decrypted text.
+
+        Parameters
+        __________
+        text_content: str
+            text to decrypt
+        char_shift: int
+            number of shifts character
+
+        Returns
+        _______
+        str
+            encrypted or decrypted text
         """
-        encrypted_chars = []
+        crypted_chars = []
         for char in text_content:
             if ord(char) in cls.upper_range:
-                encrypted_chars.append(cls.shift_char(char=char,
+                crypted_chars.append(cls.shift_char(char=char,
                                                       char_shift=char_shift,
                                                       letter_a="A",
                                                       letter_z="Z"))
             elif ord(char) in cls.lower_range:
-                encrypted_chars.append(cls.shift_char(char=char,
+                crypted_chars.append(cls.shift_char(char=char,
                                                       char_shift=char_shift,
                                                       letter_a="a",
                                                       letter_z="z"))
-        return "".join(encrypted_chars)
+        return "".join(crypted_chars)
 
     @staticmethod
     def shift_char(char: str, char_shift: int, letter_a: str, letter_z: str) -> str:
         """
-        This is static method.
-        Method takes parameter char and determines if it should be shifted.
-        Shift takes place if char is in range(ord("A"), ord("Z") + 1)
-        or in range(ord("a"), ord("z") + 1)
-        Return shifted char if it's in cipher range or return parameter char
-        :param char: str
-        :param char_shift: int
-        :param letter_a: str.
-        :param letter_z: str.
-        :return: str
+        A static method shifts character in text_content with declared char_shift if
+        it's in range between letter_a and letter_b included.
+
+        Parameters
+        __________
+        text_content: str
+            text to decrypt
+        char_shift: int
+            number of shifts character
+        letter_a: str
+            character a or A
+        letter_z: str
+            character z or Z
+
+        Returns
+        _______
+        str
+            shifted or (if char not in range) not shifted character
         """
         char_decimal = ord(char)
         if char_decimal in range(ord(letter_a), ord(letter_z) + 1):
