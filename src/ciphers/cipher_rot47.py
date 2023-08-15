1ac8ce5286e0af4c804ce842b5cc089582b9b8ad
Krzychu <czernicki.krz@gmail.com>
2023-08-15 11:17:43 +0200

    docs(cipher_rot47): create correct docstrings

--- src/ciphers/cipher_rot47.py
+++ src/ciphers/cipher_rot47.py
@@ -1,47 +1,84 @@
+"""
+A module to represent a class of cipher ROT47.
+"Shift" means shift in ASCII encoding system.
+
+Classes:
+    CipherROT47(Cipher)
+"""
 from src.ciphers.cipher import Cipher
 
 
 class CipherROT47(Cipher):
     """
-    This is class CipherROT47.
-    Inheritance from abstract class Cipher.
-    Rotate characters in word by 47 in decimal ascii notation
-    in forward(encrypt) or backward(decrypt).
-    Example: "A" -> "p",
-             "!" -> "P",
-             "z -> "K",
+    A class to represent a cipher ROT47.
+
+    Variables
+    _______________
+    cipher_range
+
+    Methods
+    _______
+    encrypt(text_content: str) -> str
+        a class method returns encrypted text
+    decrypt(text_content: str) -> str
+        a class method returns decrypted text
+    create_new_string(text_content: str, char_shift: int) -> str
+        a class method returns crypted or decrypted text
+    shift_char(char: str, char_shift: int) -> str
+        a static method returns shifted character
     """
     cipher_range = range(ord("!"), ord("~") + 1)
 
     @classmethod
     def encrypt(cls, text_content: str) -> str:
         """
-        This is class method.
-        Encrypt parameter text_content and return encrypted string.
-        :param text_content: str.
-        :return: str
+        A class method returns encrypted text.
+
+        Parameters
+        __________
+        text_content: str
+            text to encrypt
+
+        Returns
+        _______
+        str
+            encrypted text
         """
         return cls.create_new_string(text_content=text_content, char_shift=47)
 
     @classmethod
     def decrypt(cls, text_content: str) -> str:
         """
-        This is class method.
-        Decrypt parameter text_content and return decrypted string.
-        :param text_content: str.
-        :return: str
+        A class method returns decrypted text.
+
+        Parameters
+        __________
+        text_content: str
+            text to decrypt
+
+        Returns
+        _______
+        str
+            decrypted text
         """
         return cls.create_new_string(text_content=text_content, char_shift=-47)
 
     @classmethod
     def create_new_string(cls, text_content: str, char_shift: int) -> str:
         """
-        This is class method.
-        Method takes parameter text_content and shift particular letter in
-        text_content by char_shift. Then create and return new string.
-        :param text_content: str
-        :param char_shift: int
-        :return: str
+        A class method returns crypted or decrypted text.
+
+        Parameters
+        __________
+        text_content: str
+            text to decrypt
+        char_shift: int
+            number of shifts character
+
+        Returns
+        _______
+        str
+            encrypted or decrypted text
         """
         encrypted_chars = []
         for char in text_content:
@@ -51,13 +88,21 @@ class CipherROT47(Cipher):
     @classmethod
     def shift_char(cls, char: str, char_shift: int) -> str:
         """
-        This is static method.
-        Method takes parameter char and determines if it should be shifted.
-        Shift takes place if char is in range(ord("!"), ord("~") + 1)
-        Return shifted char if it's in cipher range or return parameter char
-        :param char: str
-        :param char_shift: int
-        :return: str
+        A static method shifts character in text_content
+        with declared char_shift if it's in range between
+        "!" and "~" included.
+
+        Parameters
+        __________
+        text_content: str
+            text to decrypt
+        char_shift: int
+            number of shifts character
+
+        Returns
+        _______
+        str
+            shifted or (if char not in range) not shifted character
         """
         char_decimal = ord(char)
         if char_decimal in cls.cipher_range:
