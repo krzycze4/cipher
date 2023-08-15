6ad9c160078927c2e4f61daaca2e664798a240ce
Krzychu <czernicki.krz@gmail.com>
2023-08-15 12:30:49 +0200

    docs(text): create docstrings

--- src/texts/text.py
+++ src/texts/text.py
@@ -1,21 +1,66 @@
+"""
+A module to represent a dataclass of Text.
+
+Classes:
+    Text
+"""
 from dataclasses import dataclass
 from typing import Union
 
 
 @dataclass
 class Text:
+    """
+    A class to represent a cipher ROT13.
+
+    Parameters
+    __________
+    _content: Union[None, str] = None
+    _rot_type: Union[None, str] = None
+    _status: Union[None, str] = None
+
+    Methods
+    _______
+    content() -> str
+        returns self._content
+    rot_type() -> str
+        returns self._rot_type
+    status() -> str
+        returns self._status
+    """
     _content: Union[None, str] = None
     _rot_type: Union[None, str] = None
     _status: Union[None, str] = None
 
     @property
-    def content(self):
+    def content(self) -> str:
+        """
+        A method returns self._content.
+
+        Returns
+        _______
+        self._content: str
+        """
         return self._content
 
     @property
-    def rot_type(self):
+    def rot_type(self) -> str:
+        """
+        A method returns self._rot_type.
+
+        Returns
+        _______
+        self._rot_type: str
+        """
         return self._rot_type
 
     @property
-    def status(self):
+    def status(self) -> str:
+        """
+        A method returns self._status.
+
+        Returns
+        _______
+        self._status: str
+        """
         return self._status
