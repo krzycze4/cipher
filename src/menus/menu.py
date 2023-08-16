0708216d79b0bce79ebbee23cd7f71420ceefe29
Krzychu <czernicki.krz@gmail.com>
2023-08-16 13:22:40 +0200

    refactor(menu): remove redundant methods

--- src/menus/menu.py
+++ src/menus/menu.py
@@ -1,58 +1,12 @@
-"""
-A module to represent an abstract class Menu.
-
-Classes:
-    Menu
-"""
+"""A module to represent an abstract class Menu"""
 from abc import ABC, abstractmethod
 
 
 class Menu(ABC):
-    """
-    A class to represent program menu.
-
-    Methods
-    _______
-    display_welcome() -> None
-        displays welcome text
-    display_main_menu() -> None
-        display main menu
-    display_cipher_menu() -> None
-        display cipher menu
-    """
-
-    @staticmethod
-    @abstractmethod
-    def display_welcome() -> None:
-        """
-        A static abstract method raises NotImplementedError.
-
-        Raises
-        ______
-        NotImplementedError
-        """
-        raise NotImplementedError
+    """An abstract class to represent menu."""
 
     @staticmethod
     @abstractmethod
     def display_main_menu() -> None:
-        """
-        A static abstract method raises NotImplementedError.
-
-        Raises
-        ______
-        NotImplementedError
-        """
-        raise NotImplementedError
-
-    @staticmethod
-    @abstractmethod
-    def display_cipher_menu() -> None:
-        """
-        A static abstract method raises NotImplementedError.
-
-        Raises
-        ______
-        NotImplementedError
-        """
+        """A static abstract method raises NotImplementedError."""
         raise NotImplementedError
