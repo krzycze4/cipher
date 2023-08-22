da35dc4e4d0482ca760254843a57bb111f1a9350
Krzychu <czernicki.krz@gmail.com>
2023-08-22 11:00:08 +0200

    docs(cipher_menu): modify docstrings

--- src/menus/cipher_menu.py
+++ src/menus/cipher_menu.py
@@ -1,46 +1,18 @@
-"""
-A module to represent a class CipherMenu.
-
-Classes:
-    CipherMenu
-"""
+"""A module to represent a class CipherMenu"""
 from src.menus.menu import Menu
 
 
 class CipherMenu(Menu):
-    """
-    A class to represent program menu.
-
-    Methods
-    _______
-    display_welcome() -> None
-        displays welcome text
-    display_main_menu() -> None
-        display main menu
-    display_cipher_menu() -> None
-        display cipher menu
-    """
+    """A class to represent program menu"""
 
     @staticmethod
     def display_welcome() -> None:
-        """
-        A static method displays welcome text.
-
-        Returns
-        _______
-        None
-        """
+        """A static method displays welcome text"""
         print("\nWelcome to Cipher")
 
     @staticmethod
     def display_main_menu() -> None:
-        """
-        A static method displays main menu.
-
-        Returns
-        _______
-        None
-        """
+        """A static method displays main menu"""
         print("\nMenu:")
         print("1. Encrypt texts")
         print("2. Decrypt texts")
@@ -50,13 +22,7 @@ class CipherMenu(Menu):
 
     @staticmethod
     def display_cipher_menu() -> None:
-        """
-        A static method displays cipher menu.
-
-        Returns
-        _______
-        None
-        """
+        """A static method displays cipher menu"""
         print("\nChoose cipher:")
         print("1. ROT13")
         print("2. ROT47")
