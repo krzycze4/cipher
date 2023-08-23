657660ccc6b00ccde40b48b16c285dfe00612a08
Krzychu <czernicki.krz@gmail.com>
2023-08-23 14:08:20 +0200

    test(test_cipher_rot13): fix tests

--- tests/ciphers/test_cipher_rot13.py
+++ tests/ciphers/test_cipher_rot13.py
@@ -5,118 +5,115 @@ from src.ciphers.cipher_rot13 import CipherROT13
 
 
 class TestCipherROT13:
-    @pytest.fixture
-    def cipher(self):
-        return CipherROT13
+    def setup_method(self):
+        self.cipher = CipherROT13()
 
     @pytest.mark.shift_char
-    @pytest.mark.parametrize(
-        "char, char_shift, letter_a, letter_z", [("a", 13, "a", "z")]
-    )
-    def test_should_return_lower_n_when_shift_char_lower_a(
-        self, char, char_shift, letter_a, letter_z, cipher
+    @pytest.mark.parametrize("char", ["a", "b", "k", "m"])
+    def test_should_return_shifted_plus_13_lowercase_when_shift_char_from_a_to_m(
+        self, char, char_shift=13, letter_a="a", letter_z="z"
     ):
-        shifted_char = cipher.shift_char(char, char_shift, letter_a, letter_z)
-        assert shifted_char == "n"  # nosec
+        shifted_char = self.cipher.shift_char(char, char_shift, letter_a, letter_z)
+        assert shifted_char == chr(ord(char) + char_shift)
 
     @pytest.mark.shift_char
-    @pytest.mark.parametrize(
-        "char, char_shift, letter_a, letter_z", [("n", -13, "a", "z")]
-    )
-    def test_should_return_lower_a_when_shift_char_lower_n(
-        self, char, char_shift, letter_a, letter_z, cipher
+    @pytest.mark.parametrize("char", ["n", "o", "y", "z"])
+    def test_should_return_shifted_plus_13_lowercase_when_shift_char_from_n_to_z(
+        self, char, char_shift=13, letter_a="a", letter_z="z"
     ):
-        shifted_char = cipher.shift_char(char, char_shift, letter_a, letter_z)
-        assert shifted_char == "a"  # nosec
+        shifted_char = self.cipher.shift_char(char, char_shift, letter_a, letter_z)
+        assert shifted_char == chr(char_shift + ord(char) + ord("a") - ord("z") - 1)
 
     @pytest.mark.shift_char
-    @pytest.mark.parametrize(
-        "char, char_shift, letter_a, letter_z", [("A", 13, "A", "Z")]
-    )
-    def test_should_return_upper_n_when_shift_char_upper_a(
-        self, char, char_shift, letter_a, letter_z, cipher
+    @pytest.mark.parametrize("char", ["a", "b", "k", "m"])
+    def test_should_return_shifted_minus_13_lowercase_when_shift_char_from_a_to_m(
+        self, char, char_shift=-13, letter_a="a", letter_z="z"
     ):
-        shifted_char = cipher.shift_char(char, char_shift, letter_a, letter_z)
-        assert shifted_char == "N"  # nosec
+        shifted_char = self.cipher.shift_char(char, char_shift, letter_a, letter_z)
+        assert shifted_char == chr(char_shift + ord(char) - ord("a") + ord("z") + 1)
 
     @pytest.mark.shift_char
-    @pytest.mark.parametrize(
-        "char, char_shift, letter_a, letter_z", [("N", -13, "A", "Z")]
-    )
-    def test_should_return_upper_a_when_shift_char_upper_n(
-        self, char, char_shift, letter_a, letter_z, cipher
+    @pytest.mark.parametrize("char", ["n", "o", "y", "z"])
+    def test_should_return_shifted_minus_13_lowercase_when_shift_char_from_n_to_z(
+        self, char, char_shift=-13, letter_a="a", letter_z="z"
     ):
-        shifted_char = cipher.shift_char(char, char_shift, letter_a, letter_z)
-        assert shifted_char == "A"  # nosec
+        shifted_char = self.cipher.shift_char(char, char_shift, letter_a, letter_z)
+        assert shifted_char == chr(ord(char) + char_shift)
 
     @pytest.mark.shift_char
-    @pytest.mark.parametrize(
-        "char, char_shift, letter_a, letter_z", [("9", -13, "A", "Z")]
-    )
-    def test_should_return_same_digit_when_shift_char_digit(
-        self, char, char_shift, letter_a, letter_z, cipher
+    @pytest.mark.parametrize("char", ["A", "B", "K", "M"])
+    def test_should_return_shifted_plus_13_uppercase_when_shift_char_from_A_to_M(
+        self, char, char_shift=13, letter_a="A", letter_z="Z"
     ):
-        shifted_char = cipher.shift_char(char, char_shift, letter_a, letter_z)
-        assert shifted_char == "9"  # nosec
+        shifted_char = self.cipher.shift_char(char, char_shift, letter_a, letter_z)
+        assert shifted_char == chr(ord(char) + char_shift)
 
     @pytest.mark.shift_char
-    @pytest.mark.parametrize(
-        "char, char_shift, letter_a, letter_z", [("!", -13, "A", "Z")]
-    )
+    @pytest.mark.parametrize("char", ["N", "P", "Y", "Z"])
+    def test_should_return_shifted_plus_13_uppercase_when_shift_char_from_N_to_Z(
+        self, char, char_shift=13, letter_a="A", letter_z="Z"
+    ):
+        shifted_char = self.cipher.shift_char(char, char_shift, letter_a, letter_z)
+        assert shifted_char == chr(char_shift + ord(char) + ord("a") - ord("z") - 1)
+
+    @pytest.mark.shift_char
+    @pytest.mark.parametrize("char", ["A", "B", "K", "M"])
+    def test_should_return_shifted_minus_13_uppercase_when_shift_char_from_A_to_M(
+        self, char, char_shift=-13, letter_a="A", letter_z="Z"
+    ):
+        shifted_char = self.cipher.shift_char(char, char_shift, letter_a, letter_z)
+        assert shifted_char == chr(char_shift + ord(char) - ord("a") + ord("z") + 1)
+
+    @pytest.mark.shift_char
+    @pytest.mark.parametrize("char", ["N", "P", "Y", "Z"])
+    def test_should_return_shifted_minus_13_uppercase_when_shift_char_from_N_to_Z(
+        self, char, char_shift=-13, letter_a="A", letter_z="Z"
+    ):
+        shifted_char = self.cipher.shift_char(char, char_shift, letter_a, letter_z)
+        assert shifted_char == chr(ord(char) + char_shift)
+
+    @pytest.mark.shift_char
+    @pytest.mark.parametrize("char", ["!", "~"])
     def test_should_return_same_special_sign_when_shift_char_special_sign(
-        self, char, char_shift, letter_a, letter_z, cipher
+        self, char, char_shift=-13, letter_a="A", letter_z="Z"
     ):
-        shifted_char = cipher.shift_char(char, char_shift, letter_a, letter_z)
-        assert shifted_char == "!"  # nosec
-
-    @pytest.mark.shift_char
-    @pytest.mark.parametrize(
-        "char, char_shift, letter_a, letter_z", [("z", 13, "a", "z")]
-    )
-    def test_should_return_lower_m_when_shift_lower_z(
-        self, char, char_shift, letter_a, letter_z, cipher
-    ):
-        shifted_char = cipher.shift_char(char, char_shift, letter_a, letter_z)
-        assert shifted_char == "m"  # nosec
+        shifted_char = self.cipher.shift_char(char, char_shift, letter_a, letter_z)
+        assert shifted_char == char
 
     @pytest.mark.create_new_string
-    @pytest.mark.parametrize("text_content, char_shift", [("abc", 13)])
     def test_should_return_string_when_create_new_string_execute_lowercase(
-        self, text_content, char_shift, cipher, mocker
+        self, mocker, text_content="abc", char_shift=13
     ):
         mocker.patch(
             "src.ciphers.cipher_rot13.CipherROT13.shift_char",
             side_effect=["n", "o", "p"],
         )
-        new_string = cipher.create_new_string(text_content, char_shift)
-        assert new_string == "nop"  # nosec
+        new_string = self.cipher.create_new_string(text_content, char_shift)
+        assert new_string == "nop"
 
     @pytest.mark.create_new_string
-    @pytest.mark.parametrize("text_content, char_shift", [("ABC", 13)])
     def test_should_return_string_when_create_new_string_execute_uppercase(
-        self, text_content, char_shift, cipher, mocker
+        self, mocker, text_content="ABC", char_shift=13
     ):
         mocker.patch(
             "src.ciphers.cipher_rot13.CipherROT13.shift_char",
             side_effect=["N", "O", "P"],
         )
-        new_string = cipher.create_new_string(text_content, char_shift)
-        assert new_string == "NOP"  # nosec
+        new_string = self.cipher.create_new_string(text_content, char_shift)
+        assert new_string == "NOP"
 
     @pytest.mark.encrypt
-    @pytest.mark.parametrize("text_content", ["abc"])
-    def test_should_return_string_when_encrypt_text(self, cipher, mocker, text_content):
+    def test_should_return_string_when_encrypt_text(self, mocker, text_content="abc"):
         mocker.patch(
             "src.ciphers.cipher_rot13.CipherROT13.create_new_string", return_value="nop"
         )
-        decrypted_string = cipher.encrypt(text_content)
-        assert decrypted_string == "nop"  # nosec
+        decrypted_string = self.cipher.encrypt(text_content)
+        assert decrypted_string == "nop"
 
     @pytest.mark.decrypt
-    @pytest.mark.parametrize("text_content", ["abc"])
-    def test_should_return_string_when_decrypt_text(self, cipher, mocker, text_content):
+    def test_should_return_string_when_decrypt_text(self, mocker, text_content="abc"):
         mocker.patch(
             "src.ciphers.cipher_rot13.CipherROT13.create_new_string", return_value="nop"
         )
-        decrypted_string = cipher.decrypt(text_content)
-        assert decrypted_string == "nop"  # nosec
+        decrypted_string = self.cipher.decrypt(text_content)
+        assert decrypted_string == "nop"
