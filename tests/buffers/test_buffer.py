4cb98fde4298fd89ef0061fc27ca3164675191d5
Krzychu <czernicki.krz@gmail.com>
2023-08-24 20:27:33 +0200

    docs(test_buffer): create docstrings

--- tests/buffers/test_buffer.py
+++ tests/buffers/test_buffer.py
@@ -14,12 +14,14 @@ class TestBuffer:
 
     @pytest.mark.add
     def test_should_check_len_list_when_add_once_to_empty_list(self, mocked_text):
+        """Checks adding one mocked text objects to empty buffer list"""
         len_before_add = len(self.buffer.list)
         self.buffer.add(mocked_text)
         assert len_before_add + 1 == len(self.buffer.list)
 
     @pytest.mark.add
     def test_should_check_len_list_when_add_twice_to_empty_list(self, mocked_text):
+        """Checks adding two mocked text objects to empty buffer list"""
         len_before_add = len(self.buffer.list)
         self.buffer.add(mocked_text)
         self.buffer.add(mocked_text)
@@ -27,6 +29,7 @@ class TestBuffer:
 
     @pytest.mark.add
     def test_should_check_len_list_when_add_to_not_empty_list(self, mocked_text):
+        """Checks adding one mocked text object to 2-elements buffer list"""
         self.buffer.list = [mocked_text, mocked_text]
         len_before_add = len(self.buffer.list)
         self.buffer.add(mocked_text)
@@ -34,12 +37,14 @@ class TestBuffer:
 
     @pytest.mark.clear
     def test_should_check_len_list_when_clear_empty_list(self):
+        """Checks if list is empty after clearing empty buffer list"""
         len_before_clear = len(self.buffer.list)
         self.buffer.clear()
         assert len_before_clear == len(self.buffer.list)
 
     @pytest.mark.clear
     def test_should_check_len_list_when_clear_not_empty_list(self, mocked_text):
+        """Checks if list is empty after clearing not empty buffer list"""
         self.buffer.list = [mocked_text, mocked_text]
         len_before_clear = len(self.buffer.list)
         self.buffer.clear()
