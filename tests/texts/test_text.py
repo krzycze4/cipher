a9de5571df7c11b62bf55634b2789672c0191632
Krzychu <czernicki.krz@gmail.com>
2023-08-24 20:58:58 +0200

    docs(test_text): create docstrings

--- tests/texts/test_text.py
+++ tests/texts/test_text.py
@@ -3,6 +3,7 @@ from src.texts.text import Text
 
 class TestText:
     def test_should_return_text_object(self):
+        """Check if method correctly create new Text object from dictionary"""
         text_data = {"content": "1", "rot_type": "rot", "status": "ok"}
         text = Text.create_from_dict(text_data)
         assert isinstance(text, Text)
