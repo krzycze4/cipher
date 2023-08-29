4294b1157455b78320396a24053ad8dc862c62f3
Krzychu <czernicki.krz@gmail.com>
2023-08-29 10:18:07 +0200

    refactor(cipher_rot13): refactor import

--- src/ciphers/cipher_rot13.py
+++ src/ciphers/cipher_rot13.py
@@ -4,7 +4,7 @@ A module to represent a class of cipher ROT13.
 "Shift" means shift in ASCII encoding system.
 """
 
-from src.ciphers.cipher import Cipher
+from ciphers.cipher import Cipher
 
 
 class CipherROT13(Cipher):
